1840943de1fe168cf2d8fdddcf910f14f61caeea
James N. Sears <nick@cousins-sears.com>
2013-02-25 12:11:51 -0500

    Working on es cluster

--- cookbooks/elasticsearch/recipes/restart.rb
+++ cookbooks/elasticsearch/recipes/restart.rb
@@ -6,7 +6,7 @@ node[:applications].each do |app_name, data|
   execute "restart-elasticsearch" do 
     user 'elasticsearch'
     command %Q{ 
-      echo "sleep 20 && monit -g elasticsearch restart all" | at now 
+      echo "monit -g elasticsearch restart all" | at now 
     }
   end
 end
