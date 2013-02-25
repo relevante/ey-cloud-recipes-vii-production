d4ef2d885ba4d7c80340b2dd79a7e48326ace776
James N. Sears <nick@cousins-sears.com>
2013-02-25 14:52:23 -0500

    Working on es cluster

--- cookbooks/elasticsearch/recipes/restart.rb
+++ cookbooks/elasticsearch/recipes/restart.rb
@@ -2,10 +2,10 @@ execute "ensure-elasticsearch-is-setup-with-monit" do
   command "monit reload"
 end
 
-node[:applications].each do |app_name, data|
-  execute "restart-elasticsearch" do 
-    command %Q{ 
-      sudo monit -g elasticsearch restart all
-    }
-  end
+execute "restart-elasticsearch" do 
+  command "sudo monit -g elasticsearch restart all"
+end
+
+execute "ensure-elasticsearch-is-setup-with-monit" do 
+  command "monit reload"
 end
