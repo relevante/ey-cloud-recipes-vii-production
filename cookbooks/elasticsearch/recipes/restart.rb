b38b8540ce6b900c73629b574c3304b31f079972
James N. Sears <nick@cousins-sears.com>
2013-02-25 14:58:55 -0500

    Working on es cluster

--- cookbooks/elasticsearch/recipes/restart.rb
+++ cookbooks/elasticsearch/recipes/restart.rb
@@ -2,9 +2,9 @@ execute "ensure-elasticsearch-is-setup-with-monit" do
   command "/usr/bin/monit reload"
 end
 
-execute "restart-elasticsearch" do 
-  user "root"
-  command "/usr/bin/monit -g elasticsearch restart all"
+
+execute "monit -g elasticsearch restart all" do
+  action :run
 end
 
 execute "ensure-elasticsearch-is-setup-with-monit" do 
